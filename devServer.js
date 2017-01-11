55b4a7a24179037895df0fd247b06aaa6a196fb3
Bruno Rocha Rolim <bruno_rocha2008@hotmail.com>
2017-01-11 18:12:39 -0300

    corrigido bug de porta do heroku

--- devServer.js
+++ devServer.js
@@ -22,7 +22,7 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
 
-app.listen(process.env.PORT || 7770, 'localhost', function(err) {
+app.listen(process.env.PORT || 7770, function(err) {
   if (err) {
     console.log(err)
     return
